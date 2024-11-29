ed3a86a79c4af8abc8587d151f57c67844a0dc92
yukyung <kyg0954@naver.com>
2024-11-29 10:08:51 +0900

    Remove: backend 폴더 제거

--- backend/koala_back/src/main/java/com/ssafy/domain/lecture/repository/LectureRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.domain.lecture.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ssafy.domain.lecture.model.entity.Lecture;
-
-public interface LectureRepository extends JpaRepository<Lecture, Long> {
-}
--- backend/koala_back/src/main/java/com/ssafy/domain/lecture/repository/LectureScheduleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.domain.lecture.repository;
-
-import com.ssafy.domain.lecture.model.entity.LectureSchedule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LectureScheduleRepository extends JpaRepository<LectureSchedule, Long> {
-
-}
